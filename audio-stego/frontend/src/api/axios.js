5cd545e6e06d3065193bdf9d0e4e978b097ef275
tushar <tusharanand797@gmail.com>
2026-05-09 17:46:12 +0530

    Fixed typescript error

--- audio-stego/frontend/src/api/axios.js
+++ audio-stego/frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getTokenForRequest } from './authTokenProvider'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL:import.meta.env.VITE_API_UR,
   timeout: 60000,
 })
 
